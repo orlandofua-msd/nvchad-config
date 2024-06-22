357cd1b919a1e6a1f3cdeb81d4e365097620b283
Orlando Fua III <orlando.fua@mllrdev.com>
2024-06-22 15:48:10 +0800

    feat: add nvim-dap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,9 +52,9 @@ return {
       "nvim-lua/plenary.nvim",
     },
     keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit"},
-      { "<C-j>", "<pgdown>", desc = "Scroll down main window"},
-      { "<C-k>", "<pgup>", desc = "Scroll up main window"},
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+      { "<C-j>",      "<pgdown>",         desc = "Scroll down main window" },
+      { "<C-k>",      "<pgup>",           desc = "Scroll up main window" },
     }
   },
   {
@@ -63,4 +63,75 @@ return {
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
   },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+
+      -- UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      local lldb = {
+        adapter = {
+          type = "executable",
+          command = "/usr/bin/lldb-vscode",
+          name = "lldb",
+        },
+        config = {
+          {
+            name = "Launch",
+            type = "lldb",
+            request = "launch",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
+            args = function()
+              local argument_string = vim.fn.input("Program arguments: ")
+              return vim.fn.split(argument_string, " ", true)
+            end,
+          }
+        }
+      }
+
+      dap.adapters.lldb = lldb.adapter
+
+      dap.configurations.c = lldb.config
+      dap.configurations.cpp = lldb.config
+      dap.configurations.rust = lldb.config
+      dap.configurations.cs = lldb.config
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function()
+      require("dapui").setup()
+    end,
+    requires = { "mfussenegger/nvim-dap" },
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    config = function()
+      require("nvim-dap-virtual-text").setup()
+    end,
+    requires = { "mfussenegger/nvim-dap" },
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+    end,
+  },
+  { "nvim-neotest/nvim-nio" },
 }
