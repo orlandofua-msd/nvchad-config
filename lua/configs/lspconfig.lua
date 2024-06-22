26fcbc599df8ad23161fa53cf52064891771b361
Orlando Fua III <orlando.fua@mllrdev.com>
2024-06-22 18:33:55 +0800

    feat: use omnisharp (#4)

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,10 @@
--- EXAMPLE 
+-- EXAMPLE
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "csharp_ls", "emmet_ls" }
+local servers = { "html", "cssls", "emmet_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -17,6 +17,17 @@ end
 
 -- typescript
 lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+
+lspconfig.omnisharp.setup {
+  cmd = { "dotnet", vim.fn.stdpath("data") .. "\\mason\\packages\\omnisharp\\libexec\\OmniSharp.dll" },
+  enable_editorconfig_support = true,
+  enable_roslyn_analyzers = true,
+  enable_import_completion = true,
+  organize_imports_on_format = true,
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,10 +15,14 @@ map("n", "<leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugg
 map("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
 map("n", "<leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
 map("n", "<leader>d<leader>", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
-map("n", "<F12>", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+map("n", "<F9>", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
 map("n", "<leader>d<space>", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
 map("n", "<leader>dd", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", { desc = "Debugger set conditional breakpoint" })
 map("n", "<leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
 map("n", "<leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
-map("n", "<F11>", "<Cmd>lua require'dap'.step_into()", { desc = "Step into" })
-map("n", "<F10>", "<Cmd>lua require'dap'.step_over()", { desc = "Step over" })
+map("n", "<F11>", "<Cmd>lua require'dap'.step_into()<CR>", { desc = "Step into" })
+map("n", "<F10>", "<Cmd>lua require'dap'.step_over()<CR>", { desc = "Step over" })
+
+-- C# debugging
+map("n", "<F5>", "<Cmd>lua require'csharp'.debug_project()<CR>", { desc = "Debug Project" })
+map("n", "<S-F5>", "<Cmd>lua require'csharp'.run_project()<CR>", { desc = "Run Project" })
